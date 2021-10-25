bda2d2028653c6b1a4a109dc9bc52904bb70e139
liscva <46174660+Liscva@users.noreply.github.com>
2021-10-25 11:42:41 +0800

    提交readme

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 架构采用Vue 3 + Typescript + Vite + Element-plus
 
+部分后台功能正在开发中
+
 ## 安装方式 （需要搭配后端程序使用）
-后端程序:
+[后端程序](https://github.com/Liscva/mettingroom)
+vite.config.ts中配置后端地址代理
 ~~~
 npm i
 或者
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, ConfigEnv,UserConfigExport} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock';
+// @ts-ignore
 import path from 'path'
 
 // https://vitejs.dev/config/
